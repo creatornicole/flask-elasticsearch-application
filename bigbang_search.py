735fcc7e2c2848c727fc7156448df66c1a4a8157
Nicole <ngott30@gmail.com>
2025-11-20 09:21:11 +0100

    adds first version of pages, shows all index-docs on landing page

--- bigbang_search.py
+++ bigbang_search.py
@@ -9,21 +9,39 @@ Description:
 - renders a landing page with a search form ('/')
 - processes form submissions and forwards results to a results page ('/search/results')
 - connects to an Elasticsearch instance running on localhost:9200
-
 """
 
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template, request
 from elasticsearch import Elasticsearch
+
+# load environment variables
+load_dotenv()
+ELASTIC_ADDRESS = os.getenv("ELASTIC_ADDRESS")
+INDEX_NAME = os.getenv("INDEX_NAME")
 
 # initialize flask application and configure template directory
 app = Flask(__name__, template_folder='./templates')
 # initialize elasticsearch client
-es = Elasticsearch('localhost', port=9200)
+es_client = Elasticsearch(ELASTIC_ADDRESS)
 
 # landing page: renders the search form
 @app.route('/')
 def home():
-    return render_template('search.html')
+    # fetch all documents from index
+    result = es_client.search(
+        index=INDEX_NAME,
+        body = {
+            "query": {"match_all": {}},
+            "size": 1000
+        }
+    )
+
+    # extract the actual documents
+    documents = [doc["_source"] for doc in result["hits"]["hits"]]
+
+    return render_template('search.html', documents=documents)
 
 # results page: handles form submissions and renders result template
 @app.route('/search/results', methods=['GET', 'POST'])
